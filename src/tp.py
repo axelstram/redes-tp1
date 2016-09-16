83ed5c9132d0ab0b509b3cf7f825ba96a4363108
axelstram <astraminsky@dc.uba.ar>
2016-09-15 21:26:32 -0300

    3.2.1 terminado

--- src/tp.py
+++ src/tp.py
@@ -13,11 +13,11 @@ def arp_monitor_callback(pkt):
 	
 	if ARP in pkt and pkt[ARP].op in (1,2): #who-has or is-at
 		total_packets += 1
-		
+
 		if pkt.dst == "ff:ff:ff:ff:ff:ff":
 			broadcast_counter += 1
 
-	print "broadcast: ", broadcast_counter / total_packets, " unicast: ", (total_packets - broadcast_counter) / total_packets
+		print "broadcast: ", broadcast_counter / total_packets, " unicast: ", (total_packets - broadcast_counter) / total_packets
 	
 
 
@@ -27,8 +27,9 @@ if __name__ == '__main__':
 
 	if len(sys.argv) > 1:
 		capture = rdpcap(sys.argv[1])
+
+		for pkt in capture:
+			arp_monitor_callback(pkt)
+			
 	else:
 		sniff(prn = arp_monitor_callback, filter = "arp", store = 0)
-	
-	arp_monitor_callback(capture)
-	
