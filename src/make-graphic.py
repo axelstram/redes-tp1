ba1fffdf934496b929ff0f5217095449e9f804aa
fzanollo <fzanollo@dc.uba.ar>
2016-09-16 22:47:08 -0300

    make-graphic.py now works

--- src/make-graphic.py
+++ src/make-graphic.py
@@ -1,6 +1,6 @@
 from __future__ import division
 from scapy.all import *
-import numpy as np
+import matplotlib.pyplot as plt
 import sys
 import networkx as nx
 
@@ -15,7 +15,6 @@ def crear_dicc(capture):
 				pkt_src = pkt.getlayer(ARP).psrc
 
 				src_dst[pkt_src] = pkt_dst
-	print src_dst
 	return src_dst
 
 #toma un diccionario de [ip1, ip2]
@@ -24,8 +23,6 @@ def crear_grafo(dicc):
 
 	for key, value in dicc.iteritems():
 		G.add_edge(key, value)
-	
-	print G
 	return G
 
 if __name__ == '__main__':
@@ -34,7 +31,21 @@ if __name__ == '__main__':
 	if len(sys.argv) > 1:
 		capture = rdpcap(sys.argv[1])
 
-		# dicc = crear_dicc(capture)
-		dicc = {'10.245.86.122': '10.245.85.143', '10.245.84.54': '10.245.85.143', '10.245.80.1': '10.245.85.143', '10.245.87.23': '10.245.85.143', '192.168.43.218': '192.168.43.1', '10.245.83.6': '10.245.85.143', '192.168.43.1': '192.168.43.218', '10.245.85.143': '10.245.80.3', '10.245.80.3': '10.245.85.143', '10.245.80.2': '10.245.85.143'}
+		dicc = crear_dicc(capture)
+		#dicc = {'10.245.86.122': '10.245.85.143', '10.245.84.54': '10.245.85.143', '10.245.80.1': '10.245.85.143', '10.245.87.23': '10.245.85.143', '192.168.43.218': '192.168.43.1', '10.245.83.6': '10.245.85.143', '192.168.43.1': '192.168.43.218', '10.245.85.143': '10.245.80.3', '10.245.80.3': '10.245.85.143', '10.245.80.2': '10.245.85.143'}
 		
-		crear_grafo(dicc)
+		G = crear_grafo(dicc)
+
+		pos = nx.spring_layout(G)
+
+		#https://networkx.github.io/documentation/networkx-1.10/reference/generated/networkx.drawing.nx_pylab.draw_networkx.html#networkx.drawing.nx_pylab.draw_networkx
+		nx.draw(G, pos, 
+			node_size=6000, 
+			node_shape='_', # tratamos de meterle un rectángulo pero no hubo caso --> http://matplotlib.org/api/markers_api.html
+			node_color='cyan', 
+			font_size=10, 
+			font_weight='bold', 
+			style='solid',
+			with_labels=True)
+
+		plt.savefig("Graph.png", format="PNG")
