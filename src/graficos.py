0b36273c466f44db60c1cec0da3f842220ae8a9e
axelstram <astraminsky@dc.uba.ar>
2016-12-11 13:54:09 -0300

    graficos

--- src/graficos.py
+++ src/graficos.py
@@ -5,15 +5,16 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import sys
 import operator
+from math import log
 
 
-fuente = " "
+tipoDePaqueteARP = 1
+distingoPorSource = 1
 
 
 #informacion = dicc(ip, informacion)
 #entropia = float
-#titulo = titulo papurro
-def graficar_informacion_y_entropia(informacion, entropia, titulo):
+def graficar_informacion_y_entropia(informacion, entropia, entropia_maxima):
 	sorted_data = sorted(informacion.items(), key=lambda x:x[1]) #sort por value
 	simbolos = []
 	informacion = []
@@ -28,12 +29,10 @@ def graficar_informacion_y_entropia(informacion, entropia, titulo):
 	plt.tight_layout()
 
 	#grafico informacion
-	plt.bar(range(len(informacion)), informacion, align='center')
+	plt.bar(range(len(informacion)), informacion, align='center', width=0.4)
 	plt.xticks(range(len(simbolos)), simbolos)
 
 	#grafico entropia
-	entropia_maxima = np.log2(len(informacion))
-	
 	red_patch = mpatches.Patch(color='red', label='Entropia real')
 	green_patch = mpatches.Patch(color='green', label='Entropia maxima')
 	plt.legend(handles=[red_patch, green_patch])
@@ -41,18 +40,22 @@ def graficar_informacion_y_entropia(informacion, entropia, titulo):
 	plt.plot([-1] + range(len(informacion)) + [len(informacion)], [entropia_maxima]*(len(informacion)+2), linewidth=3.0, color='green')	 
 
 	plt.show()
-	outfile = titulo.split('/')[-1].split('.')[0] + ".eps"
-	plt.savefig(outfile, format='EPS') 
+	#estirar la imagen y guardarla a mano
+
+	
+	#plt.savefig(titulo, format='EPS') 
 
 
 
 #data = dicc(ip, #repeticiones)
 def calcular_entropia(data):
-    N = float(sum(data.values()))
-    P = [i/N for i in data.values()]
-    H = sum([p*(-np.log2(p)) for p in P])
+	N = float(sum(data.values()))
+	P = [float(i)/N for i in data.values()]
+	H = sum([p*(-np.log2(p)) for p in P])
 
-    return H
+	print "entropia: ", H
+	print "entropia maxima ", np.log2(N)
+	return H
 
 
 
@@ -65,14 +68,14 @@ def calcular_informacion(data):
 		ipInf[str(ip)] = -np.log2(cantRepeticiones/N)
 
 	#print ipInf
-	#me quedo con los 10 de menor informacion (para poder encontrar el gateway)
+	#me quedo con los 8 de menor informacion (para poder encontrar el gateway)
 	ipInf_sorted_list = sorted(ipInf.items(), key=lambda x:x[1]) #sort por value
 	ipInf_sorted_dicc = {}
 	
 	#print ipInf_sorted_list
 	i = 0
 	for t in ipInf_sorted_list:
-		if i < min(10, len(ipInf_sorted_list)):
+		if i < min(8, len(ipInf_sorted_list)):
 			ipInf_sorted_dicc[str(t[0])] = t[1]
 		else:
 			break
@@ -86,7 +89,7 @@ def calcular_informacion(data):
 
 
 #toma un capture y devuelve un dicc(broadcast/unicast, cant de veces que aparece)
-def generar_data_s(capture):
+def generar_dicc_s(capture):
 	data = {}
 		
 	for pkt in capture:
@@ -109,16 +112,16 @@ def generar_data_s(capture):
 
 
 
-#toma un capture y devuelve un dicc(ip, cant de veces que aparece como src)
-def generar_data_s1(capture):
+#toma un capture y devuelve un dicc(ip, cant de veces que aparece como src o dst)
+def generar_dicc_s1(capture):
 	data = {}
 	
 	for pkt in capture:
-		if ARP in pkt and pkt[ARP].op == 1: #who-has
-			if pkt[ARP].psrc in data.keys():
-				data[pkt[ARP].psrc] += 1
+		if ARP in pkt and pkt[ARP].op == tipoDePaqueteARP:
+			if distingoPorSource:
+				data[pkt.getlayer(ARP).psrc] = data.get(pkt.getlayer(ARP).psrc, 0) + 1
 			else:
-				data[pkt[ARP].psrc] = 1
+				data[pkt.getlayer(ARP).pdst] = data.get(pkt.getlayer(ARP).pdst, 0) + 1
 
 	return data
 
@@ -128,15 +131,27 @@ def generar_data_s1(capture):
 #parametros: captura, titulo
 if __name__ == '__main__':
 	capture = rdpcap(sys.argv[1])
-	titulo = sys.argv[2]
-	fuente = sys.argv[3]
-	data = {}
 
-	if fuente == 's':
-		data = generar_data_s(capture)
+	# who-has or is-at
+	tipoDePaqueteARP = int(raw_input("Who-Has (1) or Is-At(2)?: "))
+	# source or dest
+	opcion = int(raw_input("Source (1) or Dest(2)?: "))
+
+	if opcion == 1:
+		distingoPorSource = True
+	else: 
+		distingoPorSource = False
+
+	fuente = raw_input("Fuente S o S1: ")
+	dicc = {}
+
+	if fuente == "S" or fuente == "s":
+		dicc = generar_dicc_s(capture)
 	else:
-		data = generar_data_s1(capture)
+		dicc = generar_dicc_s1(capture)
 
-	informacion = calcular_informacion(data)
-	entropia = calcular_entropia(data)
-	graficar_informacion_y_entropia(informacion, entropia, titulo="")
+	informacion = calcular_informacion(dicc)
+	entropia = calcular_entropia(dicc)
+	entropia_maxima = np.log2(float(len(dicc.values())))
+	#titulo = sys.argv[1].split('/')[-1].split('.')[0] + "_fuente_" + fuente + ".eps"
+	graficar_informacion_y_entropia(informacion, entropia, entropia_maxima)
